a7b07317f4d97c62a92af20057088502fff01d9f
SteRobWms <stephen.r.williams.87@gmail.com>
2020-09-30 13:43:15 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+
+    test = 2
+
+    if num <= 1
+        return false
+    end
+
+    while test <= num/2
+        if num % test == 0
+            return false
+        end
+        test += 1
+    end
+    
+    return true
+
+end
